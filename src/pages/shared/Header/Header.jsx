2f6af4ca47ffb255747b20872563349d92a055b9
Mintu Sikder <mintusikder15@gmail.com>
2024-03-31 00:32:06 +0600

    added marquee first

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,11 +1,29 @@
-// import React from 'react';
-
+import React from "react";
+import logo from "../../../assets/logo.png";
+import moment from "moment";
+import Marquee from "react-fast-marquee";
+import { Button, Container } from "react-bootstrap";
 const Header = () => {
-    return (
-        <div>
-            <h2>thi is header</h2>
-        </div>
-    );
+  return (
+    <Container>
+      <div className="text-center mt-4 ">
+        <img src={logo} alt="" />
+        <p className="text-secondary mt-3 ">
+          <small>Journalism Without Fear or Favour</small>
+        </p>
+        <p className="text-secondary">
+          <small>{moment().format("dddd, MMMM  DD, YYYY ")}</small>
+        </p>
+      </div>
+      <div className="d-flex bg-secondary-subtle">
+        <Button variant="danger">Latest</Button>{" "}
+        <Marquee pauseOnHover={Boolean}>
+          I can be a React component, multiple React components, or just some
+          text.
+        </Marquee>
+      </div>
+    </Container>
+  );
 };
 
 export default Header;
