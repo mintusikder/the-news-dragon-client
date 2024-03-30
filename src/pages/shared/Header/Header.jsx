e4181dae7e2b12f68c483d8c521b39646b0bcbe1
Mintu Sikder <mintusikder15@gmail.com>
2024-03-31 00:48:36 +0600

    added header section

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment";
 import Marquee from "react-fast-marquee";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 const Header = () => {
   return (
     <Container>
@@ -22,6 +22,22 @@ const Header = () => {
           text.
         </Marquee>
       </div>
+      <Navbar collapseOnSelect expand="lg" >
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#pricin">Career</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Profile</Nav.Link>
+            <Button variant="secondary">Login</Button>{' '}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </Container>
   );
 };
