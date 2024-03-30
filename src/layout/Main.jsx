dab9539b9b3f010335df1236d38f0c2a39678bfe
Mintu Sikder <mintusikder15@gmail.com>
2024-03-30 23:39:49 +0600

    addded image

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import Header from '../pages/shared/Header/Header';
-import { Outlet } from 'react-router-dom';
-import Footer from '../pages/shared/footer/Footer';
+// import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "../pages/shared/Header/Header";
+import Footer from "../pages/shared/footer/Footer";
+import { Col, Container, Row } from "react-bootstrap";
+import LeftNav from '../pages/shared/leftNav/LeftNav';
+import RightNav from '../pages/shared/rightNav/RightNav';
 
 const Main = () => {
-    return (
-        <div>
-            <Header></Header>
-            {/* <Outlet></Outlet> */}
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Header></Header>
+      <Container>
+        <Row>
+          <Col lg={3}>
+            <LeftNav></LeftNav>
+          </Col>
+          <Col lg={6}>main</Col>
+          <Col lg={3}>
+            <RightNav></RightNav>
+          </Col>
+        </Row>
+      </Container>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
--- src/pages/shared/footer/Footer.jsx
+++ src/pages/shared/footer/Footer.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+// import React from 'react';
 
 const Footer = () => {
     return (
         <div>
-            <p className='text-center text-danger'><small >©copyright all my news</small></p>
+            <p className='text-center'><small >©copyright all my news</small></p>
         </div>
     );
 };
