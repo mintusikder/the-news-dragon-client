27c578d0467137b76c5cbf6f93d58c412331cf5f
Mintu Sikder <mintusikder15@gmail.com>
2024-03-31 20:38:08 +0600

    added create news dragon servet

--- src/pages/shared/leftNav/LeftNav.jsx
+++ src/pages/shared/leftNav/LeftNav.jsx
@@ -1,6 +1,17 @@
 // import React from 'react';
 
+import { useEffect, useState } from "react";
+
 const LeftNav = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:3000/categories")
+        .then(res =>res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    },[])
+
     return (
         <div>
             <h2>Left nav</h2>
